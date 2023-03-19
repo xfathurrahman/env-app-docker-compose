976dfc58052107bb8727f77a082949f94b039054
xfathurrahman <test@localhost>
2023-03-19 18:40:34 +0700

    cache -_-

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # MYSQL - NGINX PROXY MANAGER - PHPMYADMIN
+
+# 1. make sure ur folder is writable
+# 2. copy .env.example to .env (cp .env.example .env)
+# 3. docker-compose up || docker compose up
--- init.sql
+++ init.sql
@@ -3,11 +3,11 @@
 set -e
 
 MYSQL_ROOT_PASSWORD="${MYSQL_ROOT_PASSWORD:-root}"
-MYSQL_DATABASE="${MYSQL_DATABASE:-db_npm}"
-MYSQL_USER="${MYSQL_USER:-user_npm}"
-MYSQL_PASSWORD="${MYSQL_PASSWORD:-pass_npm}"
+MYSQL_DATABASE="${NPM_DB_NAME:-db_npm}"
+MYSQL_USER="${NPM_DB_USER:-user_npm}"
+MYSQL_PASSWORD="${NPM_DB_PASSWORD:-pass_npm}"
 
 echo "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;" > /docker-entrypoint-initdb.d/init.sql
-echo "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';" >> /docker-entrypoint-initdb.d/init.sql
+echo "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED WITH mysql_native_password BY '${MYSQL_PASSWORD}';" >> /docker-entrypoint-initdb.d/init.sql
 echo "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';" >> /docker-entrypoint-initdb.d/init.sql
 echo "FLUSH PRIVILEGES;" >> /docker-entrypoint-initdb.d/init.sql
